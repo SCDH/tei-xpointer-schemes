71c799a0f07c500236280bf6d2a4bc3987566f29
Christian Lück <christian.lueck@uni-muenster.de>
2023-03-04 23:00:18 +0100

    utilities for unit tests

---
+++ core/src/test/java/org/teic/teixptr/implementation/TestUtils.java
@@ -0,0 +1,22 @@
+package org.teic.teixptr.implementation;
+
+import java.util.Iterator;
+
+import net.sf.saxon.s9api.XdmValue;
+import net.sf.saxon.s9api.XdmItem;
+import net.sf.saxon.s9api.XdmNode;
+
+
+class TestUtils {
+
+    public static XdmNode getFirstNode(XdmValue xdmValue) {
+	Iterator<XdmItem> iter = xdmValue.iterator();
+	if (iter.hasNext()) {
+	    XdmItem item = iter.next();
+	    if (item instanceof XdmNode) {
+		return (XdmNode) item;
+	    }
+	}
+	return null;
+    }
+}
