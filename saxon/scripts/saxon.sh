a66de8256c9b4e591491a47dc76142ce0b47c0cb
Christian Lück <christian.lueck@uni-muenster.de>
2023-03-06 22:34:17 +0100

    generate wrapper scripts for Saxon

---
+++ saxon/scripts/saxon.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+BUILDPATH=$SCRIPTPATH/..
+
+JARS=$BUILDPATH/lib/Saxon-HE-${saxon.version}.jar
+JARS=$JARS:$BUILDPATH/lib/xmlresolver-${xmlresolver.version}.jar
+JARS=$JARS:$BUILDPATH/lib/slf4j-api-${slf4j.version}.jar
+JARS=$JARS:$BUILDPATH/lib/slf4j-simple-${slf4j.version}.jar
+JARS=$JARS:$BUILDPATH/lib/antlr4-runtime-${antlr.version}.jar
+JARS=$JARS:$BUILDPATH/lib/tei-xpointer-grammar-${project.version}.jar
+JARS=$JARS:$BUILDPATH/${project.artifactId}-${project.version}.jar
+
+JAVAOPTS="-Ddebug=true -Dorg.slf4j.simpleLogger.defaultLogLevel=info"
+
+java $JAVAOPTS -cp $JARS $@
