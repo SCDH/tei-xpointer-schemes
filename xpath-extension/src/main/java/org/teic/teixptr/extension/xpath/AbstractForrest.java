8f02cfcc11c0a42454f1e95c1d6cdb87e75eea4b
Christian Lück <christian.lueck@uni-muenster.de>
2023-03-07 09:57:12 +0000

    definitions of XPath functions without implementation

---
+++ xpath-extension/src/main/java/org/teic/teixptr/extension/xpath/AbstractForrest.java
@@ -0,0 +1,52 @@
+package org.teic.teixptr.extension.xpath;
+
+import net.sf.saxon.lib.ExtensionFunctionDefinition;
+import net.sf.saxon.om.StructuredQName;
+import net.sf.saxon.value.SequenceType;
+import net.sf.saxon.lib.ExtensionFunctionCall;
+import net.sf.saxon.om.Sequence;
+import net.sf.saxon.trans.XPathException;
+import net.sf.saxon.expr.XPathContext;
+import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.XdmValue;
+
+
+/**
+ * This class defines the XPath function
+ *
+ * <code>xptr:forrest(nodes as node()*) as node()*</code>
+ *
+ */
+public abstract class AbstractForrest extends ExtensionFunctionDefinition {
+
+    public static final String NAME = "forrest";
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public StructuredQName getFunctionQName() {
+        return new StructuredQName(Namespace.PREFIX,
+                                   Namespace.NAMESPACE,
+                                   NAME);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public SequenceType[] getArgumentTypes() {
+        return new SequenceType[] {
+            SequenceType.NODE_SEQUENCE
+        };
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public SequenceType getResultType(SequenceType[] suppliedArgumentTypes) {
+        return SequenceType.NODE_SEQUENCE;
+    }
+
+}
