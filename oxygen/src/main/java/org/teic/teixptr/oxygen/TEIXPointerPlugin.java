8b1110980a307ef3a54c917da37f9b422e576d17
Christian Lück <christian.lueck@uni-muenster.de>
2023-03-10 13:22:49 +0100

    plugin for the oXygen XML editor

---
+++ oxygen/src/main/java/org/teic/teixptr/oxygen/TEIXPointerPlugin.java
@@ -0,0 +1,21 @@
+package org.teic.teixptr.oxygen;
+
+import ro.sync.exml.plugin.Plugin;
+import ro.sync.exml.plugin.PluginDescriptor;
+
+/**
+ * See {@link https://www.oxygenxml.com/doc/versions/22.1/ug-editor/topics/oxygen-plugin.html}
+ *
+ * Nothing to implement but the constructor.
+ */
+public class TEIXPointerPlugin extends Plugin {
+
+    /**
+     * Constructs the plugin.
+     *
+     * @@param descriptor The plugin descriptor
+     */
+    public TEIXPointerPlugin(PluginDescriptor descriptor) {
+        super(descriptor);
+    }
+}
